cfd80d65117dcb40e61f73285e108810c1d0b010
hema <hema@hema-Compaq-PC>
2012-03-27 18:48:14 -0300

    Criação das classes ListaDeUsuario e ListaDeCaronas

---
+++ SIProject/src/si1project/logic/ListaDeCaronas.java
@@ -0,0 +1,19 @@
+package si1project.logic;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class ListaDeCaronas {
+	Map<Integer, Carona> mapaCaronas = new TreeMap<Integer, Carona>();
+	
+	public Collection<Integer> localizarCarona(int idSessao, String origem,
+			String destino)  throws Exception {
+		List<Integer> caronasDisponiveis = new LinkedList<Integer>();
+		return caronasDisponiveis;
+		
+		
+	}
+}
---
+++ SIProject/src/si1project/logic/ListaDeUsuarios.java
@@ -0,0 +1,8 @@
+package si1project.logic;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class ListaDeUsuarios {
+	Map<Integer, Usuario> mapaUsu= new TreeMap<Integer, Usuario>();
+}
