8946ece6f11104471a8961c43b9c346eb2afb134
Italo <italogas@gmail.com>
2012-04-22 15:31:44 -0300

    easyaccept integrado com maven, adição de maven-compiler-plugin

---
+++ ProjetoSI/src/test/java/ccc/ufcg/edu/br/ProjetoSI/US04Test.java
@@ -0,0 +1,65 @@
+package ccc.ufcg.edu.br.ProjetoSI;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import ccc.ufcg.edu.br.ProjetoSI.SistemaCaronas;
+
+import easyaccept.EasyAcceptFacade;
+
+public class US04Test extends TestCase {
+
+	public US04Test(String testName) {
+		super(testName);
+	}
+
+	public static Test suite() {
+		return new TestSuite(US04Test.class);
+	}
+
+	public void testarEasyAcceptScript() {
+
+		List<String> files = new ArrayList<String>();
+		// Put the us1.txt file into the "test scripts" list
+		files.add("scripts/US04.txt");
+
+		// Instantiate the Monopoly Game fa�ade
+		SistemaCaronas fachada =  new SistemaCaronas();
+
+		// Instantiate EasyAccept fa�ade
+		EasyAcceptFacade eaFacade = new EasyAcceptFacade(fachada, files);
+
+		// Execute the tests
+		eaFacade.executeTests();
+
+		// Print the tests execution results
+		System.out.println(eaFacade.getCompleteResults());
+
+
+		assertTrue(eaFacade.getTotalNumberOfNotPassedTests() == 0);
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		List<String> files = new ArrayList<String>();
+		// Put the us1.txt file into the "test scripts" list
+		files.add("scripts/US04.txt");
+
+		// Instantiate the Monopoly Game fa�ade
+		SistemaCaronas fachada = new SistemaCaronas();
+
+		// Instantiate EasyAccept fa�ade
+		EasyAcceptFacade eaFacade = new EasyAcceptFacade(fachada, files);
+
+		// Execute the tests
+		eaFacade.executeTests();
+
+		// Print the tests execution results
+		System.out.println(eaFacade.getCompleteResults());
+	}
+
+}
